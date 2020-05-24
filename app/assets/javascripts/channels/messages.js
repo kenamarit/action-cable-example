3ae43669d347833efd17aaac7396e004445091a2
Ken Amarit <kenamarit@gmail.com>
2020-05-24 17:42:06 -0400

    basic structure in place. you can enter message, it generates a circle that flies across the screen, and if you hover over it it will generate the text and display for 15 sec. next step is to draw and bring in a simple background img, create a bird animation and get that working, and refactor sign up/login logic to auto redirect to the main chat room

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,6 +1,17 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {  
   received: function(data) {
-    $("#messages").removeClass('hidden')
+  	msgList.push("@" + data.user + ": " + data.message);
+  	timeList.push(0.0);
+  	xPos.push(Math.floor(Math.random() * (window.innerWidth-(window.innerWidth/2)) + window.innerWidth/4));
+  	yPos.push(Math.floor(Math.random() * (window.innerHeight-(window.innerHeight/2)) + window.innerHeight/4));
+
+  	var num = Math.floor(Math.random() * 2);
+  	if(num == 0) {
+  		dir.push(-1);	
+  	} else {
+  		dir.push(1);	
+  	}
+  	
     return $('#messages').append(this.renderMessage(data));
   },
   renderMessage: function(data) {
--- app/assets/javascripts/sketch.js
+++ app/assets/javascripts/sketch.js
@@ -1,7 +1,106 @@
-function setup() {
+var msgList = new Array();
+var timeList = new Array();
+var xPos = new Array();
+var yPos = new Array();
+var dir = new Array();
 
+
+var msgText = new Array();
+var msgTime = new Array();
+
+
+var hue;
+var saturation;
+var balance;
+
+var timeSinceStart = 0;
+
+function setup() {
+	
+  var cnv = createCanvas(windowWidth, windowHeight);
+  cnv.style('display', 'block');
+
+  hue = 306;
+  saturation = 10;
+  balance = 100;
+
+  colorMode(HSB);
+	background(hue, saturation, balance);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+	background(hue, saturation, balance);
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+	timeSinceStart += deltaTime/1000;
+
+	hue = (timeSinceStart / 8) + 306;
+	saturation = (timeSinceStart/2) + 10;
+
+	if(hue > 323) {
+		hue = 323;
+	}
+
+	if(saturation > 100) {
+		saturation = 100;
+	}
+
+  colorMode(HSB);
+	background(hue, saturation, balance);
+
+	// birds
+	for (var i = 0; i < msgList.length; i++) {
+		fill(204);
+		xPos[i] += dir[i];
+		
+		if (
+	    mouseX > xPos[i] - 20 &&
+	    mouseX < xPos[i] + 20 &&
+	    mouseY > yPos[i] - 20 &&
+	    mouseY < yPos[i] + 20
+	  ) {
+	    // mouse is over bird
+	  	//convert to msg
+	  	msgText.push(msgList[i]);
+	  	msgTime.push(0);
+	  	clearBird(i);
+	  } 
+
+	  // Draw the bird
+	  ellipse(xPos[i],yPos[i],20,20);
+
+    timeList[i] += deltaTime/1000;
+    if(timeList[i] > 10.0) {    	
+			clearBird(i);
+		}
+	}
+
+	// messages
+	var textYPos = 22;
+	for (var j = 0; j < msgText.length; j++) {
+		textSize(30);
+		text(msgText[j], 0, textYPos);
+		textYPos += 22;
+
+		msgTime[j] += deltaTime/1000;
+    if(msgTime[j] > 15.0) {    	
+			clearMsg(j);
+		}
+	}
+}
+
+function clearBird(index) {
+	msgList.splice(index,1);
+	timeList.splice(index,1);
+	xPos.splice(index,1);
+	yPos.splice(index,1);
+	dir.splice(index,1);
+}
+
+function clearMsg(index) {
+	msgText.splice(index,1);
+	msgTime.splice(index,1);
 }
