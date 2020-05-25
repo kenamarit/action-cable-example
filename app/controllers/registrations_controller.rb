9962b684857e0072bd74d0677f74857957b86ebb
Ken Amarit <kenamarit@gmail.com>
2020-05-24 20:27:08 -0400

    temp switch paths

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,8 @@ class RegistrationsController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/chatrooms/alonetogether'
+      redirect_to 'chatrooms_path'
+      # redirect_to '/chatrooms/alonetogether'
     else
       redirect_to signup_path, flash[:notice] =  user.errors.messages
     end
